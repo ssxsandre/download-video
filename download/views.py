afb7b80dc5025c7132372c145e2bf73aa5ce72e2
androlas <152226218+ssxsandre@users.noreply.github.com>
2024-06-27 13:27:02 -0300

    Atualizar o views.py

--- download/views.py
+++ download/views.py
@@ -1,11 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from pytube import YouTube
+
+from .forms import VideoForm
 
 def download_video(request):
     if request.method == 'POST':
-        url = request.POST.get("video_url")
-        yt = YouTube(url)
-        video_stream = yt.streams.get_medium_resolution()
-        video_stream.download()
-        return render(request, 'download.html', { 'video_download_complete': True, 'video_url': video_stream.default_filename })
-    return render(request, 'download.html')
+        form = VideoForm(request.POST)
+        if form.is_valid():
+            video_url = form.cleaned_data['url']
+            
+            try:
+                yt = YouTube(video_url)
+                
+                # Obtém todas as opções de stream disponíveis
+                streams = yt.streams.filter(progressive=True)
+                
+                # Renderiza o template para mostrar as opções de qualidade
+                return render(request, 'download.html', {'yt': yt, 'streams': streams})
+            
+            except Exception as e:
+                error_message = f"Erro ao processar o vídeo: {str(e)}"
+                return render(request, 'error.html', {'error_message': error_message})
+    else:
+        form = VideoForm()
+    
+    return render(request, 'index.html', {'form': form})
