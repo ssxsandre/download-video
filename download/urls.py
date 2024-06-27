0eabe060b134d608c825bc991d14baae770bf9e8
androlas <152226218+ssxsandre@users.noreply.github.com>
2024-06-27 13:30:47 -0300

    Atualizar o urls.py

--- download/urls.py
+++ download/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import download_video
 
 urlpatterns = [
-    path("download/", download_video, name="download_video"),
+    path('', download_video, name='index'),
 ]
+```
